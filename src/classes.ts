7f3606be5fb7b76e5ccb6057cd8f725732d72bd5
unknown <vallefnd@gmail.com>
2022-04-20 14:27:03 -0300

    parte de classes incompleta

--- src/classes.ts
+++ src/classes.ts
@@ -1,2 +1,42 @@
-class Usuario {    
+abstract class Usuario {    
+    constructor(
+        public id: string,
+        public nome: string,
+        public email: string,
+        public data_nasc: string,
+        public turma_id: string        
+    ){
+
+    }
+}
+
+export class Estudante extends Usuario{
+    constructor(
+        public id: string,
+        public nome: string,
+        public email: string,
+        public data_nasc: string,
+        public turma_id: string,
+        public hobbys: string[]          
+    ){
+        super(id, nome, email, data_nasc, turma_id);
+    }
+}
+
+enum {
+    JS = "JS",
+
+}
+
+export class Docente extends Usuario{
+    constructor(
+        public id: string,
+        public nome: string,
+        public email: string,
+        public data_nasc: string,
+        public turma_id: string,
+        public especialidades: string[]          
+    ){
+        super(id, nome, email, data_nasc, turma_id);
+    }
 }
