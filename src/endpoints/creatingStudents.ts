e06f352de583de8bf731a8ac281d8c6e6a9faba5
Ronald <ronald.santiago46@gmail.com>
2022-04-22 14:45:38 -0300

    Atualizações

--- src/endpoints/creatingStudents.ts
+++ src/endpoints/creatingStudents.ts
@@ -1,48 +1,51 @@
-import { Request , Response } from "express";
+import { Request, Response } from "express";
 import { Estudante, EstudanteSemHobby } from "../classes";
-import { CreatingStundetDB, ReadingHobbiesDB } from "../data";
+import { CreatingStundetDB, InsertHobbiesDB, ReadingHobbiesDB } from "../data";
 
 
 // "nome", "email", "dataNasc", "turmaID" ,"hobbys" = []
 
 
-export const creatingStudents = async (req: Request , res :Response): Promise<void> => {
+export const creatingStudents = async (req: Request, res: Response): Promise<void> => {
     try {
-        
+
         const selectHobbys = new ReadingHobbiesDB()
-        const test = await selectHobbys.readingHobbiesMeth()
+        const hobbyValuesDB = await selectHobbys.readingHobbiesMeth()
 
-        const diversao = ["patins" , "ler", "trem"]
-
-        const mappedTest = test.map((item) => {return item.nome.trim().toLowerCase()})
-                .filter((hobby) =>  diversao.find((item) => item === hobby ))
- 
-
-        console.log(mappedTest)
+        const diversao = ["patins", "ler", "trem"]
 
 
-         const {nome, email, dataNasc, turmaId:turma_id ,hobbys }: 
-            { nome:string, email:string, dataNasc : string, turmaId:string ,hobbys :string[] } = req.body
-         const id = Math.random().toString();
-         const data_nasc  = dataNasc.split("/").reverse().join("-") 
+
+
+        const id = Math.random().toString();
+        const { nome, email, dataNasc, turmaId: turma_id, hobbys }:
+            { nome: string, email: string, dataNasc: string, turmaId: string, hobbys: string[] } = req.body
+        const mapHobbys = hobbyValuesDB.map((item) => { return item.nome.trim().toLowerCase() })
+            .filter((hobby) => hobbys.find((item) => item === hobby))
+            
+            // .map((hobby) =>{  return { id:Math.random(), nome:hobby} })
+        
+        const data_nasc = dataNasc.split("/").reverse().join("-")
+        console.log('mapHobbys', mapHobbys)
 
 
         // const estudante  = 
         //    new Estudante(id, nome, email, data_nasc, turma_id ,hobbys )
-        const estudanteSemHobby = 
-            new EstudanteSemHobby(id, nome, email, data_nasc, turma_id )
-            
-            console.log("Creating students", estudanteSemHobby)
+        const estudanteSemHobby =
+            new EstudanteSemHobby(id, nome, email, data_nasc, turma_id)
+
+        console.log("Creating students", estudanteSemHobby)
 
         const CreatingStudentsDB = new CreatingStundetDB()
-        //CreatingStudentsDB.creatingStundentMeth(estudanteSemHobby)
-        res.status(201).send(estudanteSemHobby)
+        const InsertHobbies = new InsertHobbiesDB()
+        // InsertHobbies.insertHobbiesMeth(mapHobbys)
+        // CreatingStudentsDB.creatingStundentMeth(estudanteSemHobby)
+        // res.status(201).send(estudanteSemHobby)
     } catch (error: any) {
 
-        res.status(400).send(error.sqlMessage | error.message)     
+        res.status(400).send(error.sqlMessage | error.message)
     }
 }
 
 
 
-  
