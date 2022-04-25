af6f0ca1b7cf2b56835855117347dd5e859ea431
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-04-25 12:59:18 +0800

    vValidacoes para mudar tudo usuario

--- src/endpoints/changingClassUsers.ts
+++ src/endpoints/changingClassUsers.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { TurmaClasse } from "../classes";
 import { ChaggingUsersClassDB } from "../data";
 import { getALLClassesNoExpress } from "./getAllClasses";
-
-
+import { GetAllDocentesNoExpress } from "../serviceFunctions.ts/gettingAllTeachersNoExpress";
+import { GetAllDocentes } from "./gettingAllTeachers";
+import { GetAllEstudantesNoExpress } from "../serviceFunctions.ts/gettingAllStudents";
 
 
 export const changingClassUsuario = async (req: Request, res: Response): Promise<void> => {
@@ -11,14 +12,37 @@ export const changingClassUsuario = async (req: Request, res: Response): Promise
 
         const id = req.body.idUsuario as string;
         const nomeTurma = req.body.nomeTurma as string;
-        const usuario = req.body.usuario as string;
+        const usuarioTipo = req.body.usuarioTipo.toUpperCase() as string;
+
+        let isDocenteExists = [];
+        let isEstudantExists = [];
+        // Gm testing se usuario realmente existe dentro do banco de dados
+        // Testando para o Docente ou para o Estudante depende do tipoUser entrado 
+        // Tentando reutilizar a funcao. 
+        // Funcoes  get all docentes e estudantes estao no folder service. 
+        if (usuarioTipo.toUpperCase() === "DOCENTE") {
+            const allGetAllDocentesDB = await GetAllDocentesNoExpress()
+            isDocenteExists = (allGetAllDocentesDB).filter(item => {
+                if (item.id === id) {
+                    return true
+                } else return false
+            })
+        } else if (usuarioTipo === "ESTUDANTE") {
+            const GetAllEstudanteNoExpress = await GetAllEstudantesNoExpress()
+            isEstudantExists = (GetAllEstudanteNoExpress).filter(item => {
+                if (item.id === id) {
+                    return true
+                } else return false
+            })
+        } else throw new Error("Tipo de usuario errado");
+
+        const userExists: boolean = (isDocenteExists.length > 0 || isEstudantExists.length > 0)
 
         // chamando funcao para pegar todas as turmas existentes 
         // e aqui eu consigo pegar o id da turma usando o nome
         const allTurmas = await getALLClassesNoExpress();
 
-
-        if (allTurmas) {
+        if (allTurmas.length > 0) {
             const idTurma = allTurmas.filter(item => {
                 if (item.nome === nomeTurma) {
                     return true
@@ -26,26 +50,29 @@ export const changingClassUsuario = async (req: Request, res: Response): Promise
             }).map(item => item.id)
             const turma_id = idTurma[0];
 
-            if (id) {
+
+            if (turma_id && id && userExists) {
                 const chaggingStudentClass = new ChaggingUsersClassDB()
-                await chaggingStudentClass.chaggingClass(id, turma_id ,usuario )
-                console.log(id,turma_id, usuario )
-                // console.log(id , turma_id)
-                res.status(200).send(`A nova turma é ${turma_id}  , ${nomeTurma}`)
+                await chaggingStudentClass.chaggingClass(id, turma_id, usuarioTipo)
+                res.status(200).send(`A nova turma é ${turma_id}, ${nomeTurma}`)
+            } else throw new Error("Um ou mais entrada estão erradas")
 
-            }
-
-
-        }
-        //   const teste = idTurma[0] as string
-        //   console.log(idTurma[0])
-
-
+        } else throw new Error("Não há turmas no seu  Banco de Dados");
 
 
 
     } catch (error: any) {
-        res.status(400).send(error.message || error.message)
+        switch (error.message) {
+            case "Não a turmas no seu  Banco de Dados":
+                res.status(400).send(error.message)
+                break;
+            case "Um ou mais entrada estão erradas":
+                res.status(422).send(error.message)
+                break;
+            default:
+                res.status(500).send(error.sqlMessage || error.message)
+
+        }
     }
 
 
